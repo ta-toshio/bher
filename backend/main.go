e6a83fe8991c10a02ac5f14abe217937085ece41
ta-toshio <ta.toshio.info@gmail.com>
2021-10-27 14:25:57 +0900

    Use sql client

--- backend/main.go
+++ backend/main.go
@@ -1,17 +1,37 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
 
+	_ "github.com/mattn/go-sqlite3"
+
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+
+	"github.com/ta-toshio/bherb/ent"
 	"github.com/ta-toshio/bherb/graph"
 	"github.com/ta-toshio/bherb/graph/generated"
 )
 
 const defaultPort = "8080"
+
+func newClient() *ent.Client {
+	// Create a Sqlite3 database connection.
+	client, err := ent.Open("sqlite3", "file:ent?mode=memory&cache=shared&_fk=1")
+	if err != nil {
+		log.Fatalf("failed opening connection to sqlite: %v", err)
+	}
+
+	// Run the auto migration tool.
+	if err := client.Schema.Create(context.Background()); err != nil {
+		log.Fatalf("failed creating schema resources: %v", err)
+	}
+
+	return client
+}
 
 func main() {
 	port := os.Getenv("PORT")
@@ -19,7 +39,12 @@ func main() {
 		port = defaultPort
 	}
 
-	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+	client := newClient()
+	defer client.Close()
+
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{
+		Client: client,
+	}}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
