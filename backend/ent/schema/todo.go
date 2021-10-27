cb300181a627d18ce9ffea0ce5cd8e2b6f699c4c
ta-toshio <ta.toshio.info@gmail.com>
2021-10-27 14:03:41 +0900

    Add ent model

---
+++ backend/ent/schema/todo.go
@@ -0,0 +1,26 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+)
+
+// Todo holds the schema definition for the Todo entity.
+type Todo struct {
+	ent.Schema
+}
+
+// Fields of the Todo.
+func (Todo) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("text").
+			NotEmpty(),
+		field.Bool("done").
+			Default(false),
+	}
+}
+
+// Edges of the Todo.
+func (Todo) Edges() []ent.Edge {
+	return nil
+}
