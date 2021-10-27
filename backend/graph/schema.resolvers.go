1aeda0ab97efa23130bd7b28461cc60a86e117eb
ta-toshio <ta.toshio.info@gmail.com>
2021-10-27 15:24:10 +0900

    Update resolve

--- backend/graph/schema.resolvers.go
+++ backend/graph/schema.resolvers.go
@@ -5,22 +5,56 @@ package graph
 
 import (
 	"context"
-	"fmt"
 
+	"github.com/ta-toshio/bherb/ent"
+	"github.com/ta-toshio/bherb/ent/user"
 	"github.com/ta-toshio/bherb/graph/generated"
 	"github.com/ta-toshio/bherb/graph/model"
 )
 
-func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
-	panic(fmt.Errorf("not implemented"))
+func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*ent.Todo, error) {
+	// Fetch User based on UserName input parameter.
+	usr, err := r.Client.User.Query().Where(
+		user.NameEQ(input.UserName),
+	).First(ctx)
+
+	// Check errors
+	if err != nil {
+		// If error is not not found, return the error.
+		if !ent.IsNotFound(err) {
+			return nil, err
+		}
+
+		// Otherwise create the User.
+		usr, err = r.Client.User.Create().
+			SetName(input.UserName).
+			Save(ctx)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	// Create the new Todo.
+	return r.Client.Todo.Create().
+		SetText(input.Text). // Set text
+		SetUser(usr).        // Set User
+		Save(ctx)            // Save it
 }
 
-func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
-	panic(fmt.Errorf("not implemented"))
+func (r *queryResolver) Todos(ctx context.Context) ([]*ent.Todo, error) {
+	// Returns all the Todo.
+	return r.Client.Todo.Query().All(ctx)
 }
 
-func (r *queryResolver) UserTodos(ctx context.Context, name string) ([]*model.Todo, error) {
-	panic(fmt.Errorf("not implemented"))
+func (r *queryResolver) UserTodos(ctx context.Context, name string) ([]*ent.Todo, error) {
+	// Query all Users in database.
+	return r.Client.User.Query().
+		Where(
+			// Filter only User that exactly match the name parameter.
+			user.NameEQ(name),
+		).
+		QueryTodos(). // Query all Todos of the found User.
+		All(ctx)      // Returns all Todos.
 }
 
 // Mutation returns generated.MutationResolver implementation.
