06bb53756ec7ea15fb65cd73841e1c434c818470
ta-toshio <ta.toshio.info@gmail.com>
2021-11-07 16:57:35 +0900

    Add apollo client

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,7 +1,16 @@
+import React from 'react'
 import type { AppProps } from 'next/app'
+import { ApolloProvider } from '@apollo/client'
+import { useApollo } from '../app/withApollo'
 import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const apolloClient = useApollo(pageProps)
+
+  return (
+    <ApolloProvider client={apolloClient}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
 export default MyApp
